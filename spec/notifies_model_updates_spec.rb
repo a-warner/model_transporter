ed19d95564a4fde9fc9f742255bc086e5f7a7b46
Andrew Warner <wwarner.andrew@gmail.com>
2020-10-21 09:19:30 -0400

    Validate specific arguments to enqueue_model_updates

--- spec/notifies_model_updates_spec.rb
+++ spec/notifies_model_updates_spec.rb
@@ -3,15 +3,26 @@ RSpec.describe ModelTransporter::NotifiesModelUpdates do
     expect(described_class).not_to be nil
   end
 
-  it 'enqueues model updates' do
-    expect(ModelTransporter::BatchModelUpdates).to receive(:enqueue_model_updates)
+  it "doesn't enqueue model updates for unregistered models" do
+    expect(ModelTransporter::BatchModelUpdates).not_to receive(:enqueue_model_updates)
+    user = User.create(username: 'andrew')
+  end
 
+  it 'enqueues model updates on create' do
     user = User.create(username: 'andrew')
 
-    blog_post = BlogPost.create(
+    blog_post = BlogPost.new(
       author: user,
       title: 'Hello, world!'
     )
+
+    expect(ModelTransporter::BatchModelUpdates).to receive(:enqueue_model_updates) { |broadcasting_key, payload|
+      expect(broadcasting_key).to eq AdminChannel.broadcasting_for('all')
+
+      expect(payload[:creates]['blog_posts']).to match(hash_including(blog_post.id => blog_post))
+    }
+
+    blog_post.save
 
     expect(blog_post.id).not_to be nil
   end
