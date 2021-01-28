d676926a10bd0f4b3dcc10a48ce37f062f4dd86b
Andrew Warner <wwarner.andrew@gmail.com>
2021-01-27 21:39:02 -0500

    Configuration option for which id to use
    - E.g. if keying off of a `hashid` or slug/etc

--- lib/model_transporter/notifies_model_updates.rb
+++ lib/model_transporter/notifies_model_updates.rb
@@ -7,8 +7,8 @@ module ModelTransporter::NotifiesModelUpdates
   end
 
   class_methods do
-    def notifies_model_updates(channel:, on: %i(create update destroy))
-      self.notifies_model_updates_options = { channel: channel }
+    def notifies_model_updates(channel:, id_key: :id, on: %i(create update destroy))
+      self.notifies_model_updates_options = { channel: channel, id_key: id_key }
 
       if on.include?(:create)
         after_create_commit -> { notify_model_updates(:creates, self) }
@@ -26,9 +26,14 @@ module ModelTransporter::NotifiesModelUpdates
 
   def notify_model_updates(update_type, model_state)
     channel = instance_exec(&(self.class.notifies_model_updates_options[:channel]))
+    id_key = self.public_send(self.class.notifies_model_updates_options[:id_key])
 
     payload = { creates: {}, updates: {}, deletes: {} }
-    payload[update_type] = { self.class.name.pluralize.underscore => { self.id => model_state } }
+    payload[update_type] = {
+      self.class.name.pluralize.underscore => {
+        id_key => model_state
+      }
+    }
 
     ModelTransporter::BatchModelUpdates.enqueue_model_updates(
       channel,
